a1f08950557015ecfb667e8acdbcb73323d29aec
Furkan Vural <furkanvural52@gmail.com>
2024-01-03 22:25:18 +0300

    Add save bar button item and configure checkmark

--- New Hall Cati/New Hall Cati/Screens/AdmingPage/AdminPageVC.swift
+++ New Hall Cati/New Hall Cati/Screens/AdmingPage/AdminPageVC.swift
@@ -1,10 +1,3 @@
-//
-//  AdminPageVC.swift
-//  New Hall Cati
-//
-//  Created by furkan vural on 3.01.2024.
-//
-
 import UIKit
 
 protocol AdminPageProtocol {
@@ -16,6 +9,7 @@ protocol AdminPageProtocol {
     
     func configureSegmentedController()
     func configureTableView()
+    func configureNavigationSaveButton()
 }
 
 final class AdminPageVC: UIViewController, AdminPageProtocol {
@@ -30,13 +24,19 @@ final class AdminPageVC: UIViewController, AdminPageProtocol {
     private var segmentedController: CustomSegmentedController!
     private var tableView: UITableView!
     
+//    MARK: - Mock Data
+    var food = ["1F", "2F", "3F", "4F"]
+    var dessert = ["1D", "2D", "3D", "4D"]
+    var drink = ["1Dr", "2Dr", "3Dr", "4Dr"]
     
+    var selectedMenu: Set<String> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureSegmentedController()
         configureTableView()
+        configureNavigationSaveButton()
         
         
     }
@@ -57,20 +57,28 @@ final class AdminPageVC: UIViewController, AdminPageProtocol {
     
     func configureTableView() {
         tableView = UITableView()
-        
+        tableView.register(CellView.self, forCellReuseIdentifier: CellView.identifier)
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.allowsMultipleSelection = true
+        tableView.allowsSelection = true
         
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: segmentedController.bottomAnchor, constant: 5),
+            tableView.topAnchor.constraint(equalTo: segmentedController.bottomAnchor, constant: 10),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.heightAnchor.constraint(equalTo: view.heightAnchor)
         ])
+    }
+    
+    func configureNavigationSaveButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveNewMenu))
+    }
+    
+    @objc func saveNewMenu() {
+        print("Save new menu item \(selectedMenu)")
     }
 }
 
@@ -81,12 +89,40 @@ extension AdminPageVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return food.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Deneme"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellView.identifier, for: indexPath) as? CellView else {
+            fatalError("Could not dequeue cell with identifier: \(CellView.identifier)")
+        }
+
+        cell.set(with: food[indexPath.row])
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let cell = tableView.cellForRow(at: indexPath)
+        let selectedItem = food[indexPath.row]
+        
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+            if selectedMenu.contains(selectedItem) {
+                selectedMenu.remove(selectedItem)
+                print(selectedMenu)
+            }
+            
+        } else {
+            cell?.accessoryType = .checkmark
+            print(food[indexPath.row])
+            selectedMenu.insert(food[indexPath.row])
+            print(selectedMenu)
+        }
+        
     }
 }
---
+++ New Hall Cati/New Hall Cati/Screens/AdmingPage/TableViewCell/CellView.swift
@@ -0,0 +1,55 @@
+//
+//  CellView.swift
+//  New Hall Cati
+//
+//  Created by furkan vural on 3.01.2024.
+//
+
+import UIKit
+
+class CellView: UITableViewCell {
+
+    static let identifier = "AdminCellIdentifier"
+    
+    private let nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.textColor = .black
+        return nameLabel
+    }()
+    
+    private let priceLabel: UILabel = {
+        let priceLabel = UILabel()
+        priceLabel.textColor = .black
+        return priceLabel
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(with product: String) {
+        nameLabel.text = product
+        priceLabel.text = product
+    }
+    
+    private func configure() {
+        self.contentView.addSubview(nameLabel)
+        self.contentView.addSubview(priceLabel)
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            priceLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            priceLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+        ])
+    }
+    
+}
