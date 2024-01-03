b4d83f4bbfdfc23c1f0b90b56fa555bfb0358ab9
Furkan Vural <furkanvural52@gmail.com>
2024-01-03 13:25:00 +0300

    Add tableview to adming page

--- New Hall Cati/New Hall Cati/Screens/AdmingPage/AdminPageVC.swift
+++ New Hall Cati/New Hall Cati/Screens/AdmingPage/AdminPageVC.swift
@@ -13,6 +13,9 @@ protocol AdminPageProtocol {
     var allDessertList: [String] { get }
     var allDrinkList: [String] { get }
     var viewModel: AdminPageViewModel { get }
+    
+    func configureSegmentedController()
+    func configureTableView()
 }
 
 final class AdminPageVC: UIViewController, AdminPageProtocol {
@@ -25,17 +28,20 @@ final class AdminPageVC: UIViewController, AdminPageProtocol {
 
 //    MARK: - UI Elements
     private var segmentedController: CustomSegmentedController!
+    private var tableView: UITableView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureSegmentedController()
         view.backgroundColor = .systemBackground
+        configureSegmentedController()
+        configureTableView()
+        
         
     }
     
-    private func configureSegmentedController() {
+     func configureSegmentedController() {
         segmentedController = CustomSegmentedController(frame: .zero)
         let items = ["Ana Yemek", "Tatlılar", "İçecekler"]
         segmentedController.setItems(items, startingIndex: 0)
@@ -47,5 +53,40 @@ final class AdminPageVC: UIViewController, AdminPageProtocol {
             segmentedController.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
             segmentedController.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5)
         ])
+    }
+    
+    func configureTableView() {
+        tableView = UITableView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsMultipleSelection = true
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: segmentedController.bottomAnchor, constant: 5),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+    }
+}
+
+extension AdminPageVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Deneme"
+        return cell
     }
 }
