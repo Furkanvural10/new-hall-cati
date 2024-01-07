ee0feedf7b5960e9c1b4141de42e1fd502668e02
Furkan Vural <furkanvural52@gmail.com>
2024-01-07 14:51:34 +0300

    Add firebase to project

--- New Hall Cati/New Hall Cati/Support/AppDelegate.swift
+++ New Hall Cati/New Hall Cati/Support/AppDelegate.swift
@@ -1,12 +1,6 @@
-//
-//  AppDelegate.swift
-//  New Hall Cati
-//
-//  Created by furkan vural on 28.12.2023.
-//
-
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
